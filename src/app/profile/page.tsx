84209d33b1b34e7022fd05fab8d79c07d972ee91
techwarq <102250604+techwarq@users.noreply.github.com>
2024-07-16 23:40:03 +0530

    first commit for full profile section

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,34 +1,45 @@
 import { redirect } from 'next/navigation';
 import { authOptions, getAuthSession } from '@/lib/auth';
-import UserActivities from '@/components/UserActivities';
 import { db } from '@/lib/db';
-
-import Link from 'next/link'
-import { User } from 'next-auth'
-import { UserAvatar } from '../../components/UserAvatar'
+import { UserAvatar } from '@/components/UserAvatar';
+import { INFINITE_SCROLL_PAGINATION_RESULTS } from '@/config';
+import Post from '@/components/Post';
 
 interface ProfileProps {
   params: { username: string }
 }
 
 export default async function ProfilePage({ params: { username } }: ProfileProps) {
-  const session = await getAuthSession()
+  const session = await getAuthSession();
 
   if (!session?.user) {
-    redirect(authOptions?.pages?.signIn || '/login')
+    redirect(authOptions?.pages?.signIn || '/login');
   }
 
   const user = await db.user.findFirst({
     where: { username },
-  })
+    include: {
+      posts: {
+        include: {
+          author: true,
+          votes: true,
+          comments: true,
+          subnerds: true,
+        },
+        orderBy: {
+          createdAt: 'desc',
+        },
+        take: INFINITE_SCROLL_PAGINATION_RESULTS,
+      },
+    },
+  });
 
   if (!user) {
-    // Handle the case where the user is not found
-    return <div>User not found</div>
+    return <div>User not found</div>;
   }
 
   return (
-    <div className='flex w-full flex-col justify-start'>
+    <div className='flex w-full text-green-600 flex-col justify-start'>
       <div className='flex items-center justify-between'>
         <div className='flex items-center gap-3'>
           <div className='relative'>
@@ -44,6 +55,26 @@ export default async function ProfilePage({ params: { username } }: ProfileProps
             <p className='text-base-medium text-gray-1'>{user.username}</p>
           </div>
         </div>
+      </div>
+
+      <div className='mt-4'>
+        <h3 className='text-heading3-bold text-light-1'>Posts</h3>
+        {user.posts.length ? (
+          <ul className='mt-4'>
+            {user.posts.map((post) => (
+              <li key={post.id} className='mb-4'>
+                <Post
+                  post={post}
+                  votesAmt={post.votes.length}
+                  subredditName={post.subnerds.name}
+                  commentAmt={post.comments.length}
+                />
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p className='text-base-medium text-gray-1'>No posts found.</p>
+        )}
       </div>
     </div>
   );
