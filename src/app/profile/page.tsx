2429c48275125ade369bf132ee224701832c9a3f
techwarq <102250604+techwarq@users.noreply.github.com>
2024-07-16 23:14:02 +0530

    first commit for full profile section

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,56 +1,50 @@
-
 import { redirect } from 'next/navigation';
 import { authOptions, getAuthSession } from '@/lib/auth';
 import UserActivities from '@/components/UserActivities';
 import { db } from '@/lib/db';
 
-
-
 import Link from 'next/link'
 import { User } from 'next-auth'
 import { UserAvatar } from '../../components/UserAvatar'
 
-interface ProfileProps extends React.HTMLAttributes<HTMLDivElement> {
-    user: Pick<User, 'name' | 'image' | 'email'>
-    params: {username: string}
+interface ProfileProps {
+  params: { username: string }
+}
+
+export default async function ProfilePage({ params: { username } }: ProfileProps) {
+  const session = await getAuthSession()
+
+  if (!session?.user) {
+    redirect(authOptions?.pages?.signIn || '/login')
   }
 
+  const user = await db.user.findFirst({
+    where: { username },
+  })
 
-export default async function ProfilePage({params: {username}}: ProfileProps) {
-    const session = await getAuthSession()
+  if (!user) {
+    // Handle the case where the user is not found
+    return <div>User not found</div>
+  }
 
-    if (!session?.user) {
-        redirect(authOptions?.pages?.signIn || '/login')
-      }
-    
-
-    const User = await db.user.findFirst({
-        where : {username},
-        
-    })
- 
-
-
-    
-
-  
-
-    return (
-      <div className='flex w-full flex-col justify-start'>
-        <div className='flex items-center justify-between'>
-            <div className='flex items-center gap-3'>
-                <div className='relative '>
-              
-                </div>
-                <div className='flex-1'>
-            <h2 className='text-left text-heading3-bold text-light-1'>
-              
-            </h2>
-            <p className='text-base-medium text-gray-1'>{User?.username}</p>
+  return (
+    <div className='flex w-full flex-col justify-start'>
+      <div className='flex items-center justify-between'>
+        <div className='flex items-center gap-3'>
+          <div className='relative'>
+            <UserAvatar
+              user={{ name: user.name || null, image: user.image || null }}
+              className='h-8 w-8'
+            />
           </div>
-            </div>
+          <div className='flex-1'>
+            <h2 className='text-left text-heading3-bold text-light-1'>
+              {user.name}
+            </h2>
+            <p className='text-base-medium text-gray-1'>{user.username}</p>
+          </div>
         </div>
       </div>
-    );
-  
+    </div>
+  );
 }
